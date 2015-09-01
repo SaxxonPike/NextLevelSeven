86b1d39baae71157c38d42c19df620179b66d95b
Tony Konzel <saxxonpike@gmail.com>
2015-09-01 12:12:29 -0500

    Error message generator documentation added.

--- NextLevelSeven/Diagnostics/ErrorMessages.cs
+++ NextLevelSeven/Diagnostics/ErrorMessages.cs
@@ -12,14 +12,32 @@ namespace NextLevelSeven.Diagnostics
     /// </summary>
     static public partial class ErrorMessages
     {
+        /// <summary>
+        /// Base class for error message languages.
+        /// </summary>
         abstract private class ErrorMessageLanguage
         {
+            /// <summary>
+            /// Get the error message for this language.
+            /// </summary>
+            /// <param name="code">Error code to get the message for.</param>
+            /// <returns>Error string.</returns>
             public abstract string GetMessage(ErrorCode code);
         }
 
+        /// <summary>
+        /// Language to fall back when the selected language does not have a translation.
+        /// </summary>
         private static readonly ErrorMessageLanguage FallbackLanguage = new English();
 
+        /// <summary>
+        /// Backing store for Language.
+        /// </summary>
         private static ErrorMessageLanguage _language;
+
+        /// <summary>
+        /// Currently selected language.
+        /// </summary>
         static private ErrorMessageLanguage Language
         {
             get
@@ -32,6 +50,11 @@ namespace NextLevelSeven.Diagnostics
             }
         }
 
+        /// <summary>
+        /// Get an error message for the currently selected language.
+        /// </summary>
+        /// <param name="code">Error code to get the message for.</param>
+        /// <returns></returns>
         static public string Get(ErrorCode code)
         {
             var message = Language.GetMessage(code);
@@ -43,6 +66,10 @@ namespace NextLevelSeven.Diagnostics
             return string.Format("{0} (NL7-{1})", message ?? "Unknown error.", (int)code);
         }
 
+        /// <summary>
+        /// Set the selected language for error messages.
+        /// </summary>
+        /// <param name="language">Two-letter language code. If more than two characters are passed, only the first two are used.</param>
         static public void SetLanguage(string language = null)
         {
             var cultureName = language ?? CultureInfo.CurrentCulture.Name.ToLowerInvariant();
