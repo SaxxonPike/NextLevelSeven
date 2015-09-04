c9a7ead2d774394167b8cef5cdf2698c27a63c0a
Tony Konzel <saxxonpike@gmail.com>
2015-09-04 11:58:44 -0500

    More specific testing on message generator.

--- NextLevelSeven.Test/MessageGeneration/MessageGeneratorTests.cs
+++ NextLevelSeven.Test/MessageGeneration/MessageGeneratorTests.cs
@@ -16,8 +16,8 @@ namespace NextLevelSeven.Test.MessageGeneration
             Assert.AreEqual("|", message[1][1].Value, @"MSH-1 is not the pipe character.");
             Assert.AreEqual("^~\\&", message[1][2].Value, @"MSH-2 is not the standard set of encoding characters.");
             Assert.IsNotNull(message.ProcessingId, @"Processing ID is null.");
-            Assert.IsNotNull(message.Type, @"Message type is null.");
-            Assert.IsNotNull(message.TriggerEvent, @"Message trigger event is null.");
+            Assert.AreEqual(message.Type, "ADT", @"Message type is not correct.");
+            Assert.AreEqual(message.TriggerEvent, "A01", @"Message type is not correct.");
             Assert.IsNotNull(message.Version, @"Version number is null.");
         }
     }
